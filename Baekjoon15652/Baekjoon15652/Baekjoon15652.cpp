aa9cf53c0d7b791765a9ed7de6e6dd2b96b12ca7
rnrytjr2 <43197846+rnrytjr2@users.noreply.github.com>
2022-09-08 22:16:20 +0900

    solved

---
+++ Baekjoon15652/Baekjoon15652/Baekjoon15652.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+void sequence(int N, int M,int lastindex, string answer);
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	sequence(N, M,1 ,"");
+}
+void sequence(int N, int M,int lastindex, string answer)
+{
+	if (answer.size() == M + M - 1)
+	{
+		cout << answer << "\n";
+	}
+	else
+	{
+		for (int i = 1; i < N + 1; i++)
+		{
+			if (lastindex <= i)
+			{
+				string answertemp = answer;
+				if (answer.size() != 0)
+				{
+					answertemp += " ";
+				}
+				sequence(N, M, i, answertemp + to_string(i));
+			}
+		}
+	}
+}
